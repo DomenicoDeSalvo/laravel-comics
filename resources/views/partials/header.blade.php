3e9f73a216c605e058e2c99f749301cc440c1e66
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-22 01:15:18 +0200

    Header completed

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <ul class='d-flex list-unstyled'>
+            <li>
+                <a href="{{route('home')}}">Home</a>
+            </li>
+            <li>
+                <a href="{{route('comics')}}">Comics</a>
+            </li>
+        </ul>
+    </nav>
+</header>
