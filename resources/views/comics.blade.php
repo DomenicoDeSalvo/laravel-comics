8d82bc3b17d9b502216994b357840fb791bcf500
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-23 21:01:09 +0200

    Added columns containing cards

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,6 +4,21 @@
 
 @section('content')
     <div class="container">
-        <h1>COMICS</h1>
+        <h1>COMICS</h1>        
+    </div>
+    <div class="container">
+        <div class="row row-cols-6">
+            @foreach ($comics as $comic)
+                <div class="col my-4 d-flex align-items-stretch">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">{{$comic['title'];}}</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
