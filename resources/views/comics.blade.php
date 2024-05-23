034a7256737baa0571852620547d3f8f90909caa
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-23 21:20:44 +0200

    Cards correctly display the comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -9,12 +9,12 @@
     <div class="container">
         <div class="row row-cols-6">
             @foreach ($comics as $comic)
-                <div class="col my-4 d-flex align-items-stretch">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                <div class="col  my-4 d-flex align-items-stretch">
+                    <div class="card flex-fill">
+                        <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title text-center">{{$comic['title'];}}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">{{$comic['series'];}}</p>
                         </div>
                     </div>
                 </div>
